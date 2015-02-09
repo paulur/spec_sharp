399904d8d90a8922f0dc8f4e375a5ff97197195c
paulur <paulur@gmail.com>
2015-02-09 13:37:56 -0800

    add root-dir to constant

--- spec/sharp/CONST.py
+++ spec/sharp/CONST.py
@@ -8,6 +8,7 @@ class CONST(object):
     '''
     constants used in this application
     '''
-    KEYWORD_SERACH_CONFIG   = "/home/paul/workspace/spec_sharp/keyword-search-config.xml"
-    WEBLOG_ENTRIES          = "/home/paul/workspace/spec_sharp/weblog-entries.txt"
-    WEBLOG_URLS             = "/home/paul/workspace/spec_sharp/weblog-urls.txt"
+    ROOT_DIR                = "/home/paul/projects/spec_sharp/"
+    KEYWORD_SERACH_CONFIG   = ROOT_DIR + "keyword-search-config.xml"
+    WEBLOG_ENTRIES          = ROOT_DIR + "weblog-entries.txt"
+    WEBLOG_URLS             = ROOT_DIR + "weblog-urls.txt"
