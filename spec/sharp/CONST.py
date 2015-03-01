143d2b9340021d4196fa3fe451a0ee2f3cac7fe9
paulur <paulur@gmail.com>
2015-02-28 19:15:09 -0600

    put all data files under file directory.

--- spec/sharp/CONST.py
+++ spec/sharp/CONST.py
@@ -8,7 +8,7 @@ class CONST(object):
     '''
     constants used in this application
     '''
-    ROOT_DIR                = "C:\\Users\\wli001\\git\\spec_sharp\\"
+    ROOT_DIR                = "C:\\Users\\wli001\\git\\spec_sharp\\file\\"
     KEYWORD_SERACH_CONFIG   = ROOT_DIR + "keyword-search-conf.xml"
     WEBLOG_ENTRIES          = ROOT_DIR + "weblog-entries.txt"
     WEBLOG_URLS             = ROOT_DIR + "weblog-urls.txt"
