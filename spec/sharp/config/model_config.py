4678c39b36a0f02fae119268a5e64f7f69c39f8d
paulur <paulur@gmail.com>
2015-03-07 22:12:08 -0600

    url model ts-detector

--- spec/sharp/config/model_config.py
+++ spec/sharp/config/model_config.py
@@ -26,20 +26,24 @@ class ModelConfig(object):
     classdocs
     '''
 
-    def __init__(self, m_name='', m_desc='', l_type='', l_file=''):
+    def __init__(self, m_name='', m_desc='', l_type='', l_file='', l_formatter='', t_formatter=''):
         '''
         Constructor
         '''
-        self.model_name = m_name
-        self.model_desc = m_desc
-        self.log_type   = l_type
-        self.log_file    = l_file
+        self.model_name     = m_name
+        self.model_desc     = m_desc
+        self.log_type       = l_type
+        self.log_file       = l_file
+        self.log_formatter  = l_formatter
+        self.time_formatter = t_formatter
         
     def __str__(self):
         return 'model_name: ' + self.model_name + \
                 '\nmodel_desc: ' + self.model_desc + \
                 '\nlog_type: ' + self.log_type + \
-                '\nlog_file: ' + self.log_file
+                '\nlog_file: ' + self.log_file + \
+                '\nlog_formatter: ' + self.log_formatter + \
+                '\ntime_formatter: ' + self.time_formatter
         
         
 class ModelConfigBuilder(object):
@@ -62,12 +66,15 @@ class ModelConfigBuilder(object):
                 l_type = text
             elif tag == 'log-file':
                 _l_loc = text
-                '''replace $log_dir$ with real dir'''
                 l_loc = CONST.LOG_DIR + _l_loc
+            elif tag == 'log-formatter':
+                l_formatter = text
+            elif tag == 'time-formatter':
+                t_formatter = text
             else:
                 '''done'''
         
-        return ModelConfig(m_name, m_desc, l_type, l_loc)
+        return ModelConfig(m_name, m_desc, l_type, l_loc, l_formatter, t_formatter)
                 
 
 mcb =  ModelConfigBuilder()
--- spec/sharp/model/url_alert_model.py
+++ spec/sharp/model/url_alert_model.py
@@ -12,63 +12,141 @@ from collections import OrderedDict
 from spec.sharp.CONST import CONST
 from spec.sharp.config.model_config import *
 
-class URLModelItem(object):
-    '''constructor
-    :param t    timestamp of the log event
-    :param m    http request method
-    :param p    request path
-    :param p_list    request parameter list in order
-    '''
-    def __init__(self, t, m, p, p_list):
+class URLModelItem(object):        
+    def __init__(self, t='', m='', p='', pl=[]):
+        '''constructor'''
         self.timestamp  = t
         self.method     = m
         self.path       = p
-        self.param_list = p_list
-    
-    def __str__(self, ):
-        return 'timestamp: ' + self.timestamp + '\tmethod:' + self.method + '\tpath: ' + self.path + '\tp_lsit: ' + str(self.param_list) 
+        self.param_list = pl
+       
+    def __str__(self):
+        return 'timestamp: ' + self.timestamp + '\tmethod:' + self.method + '\tpath: ' + self.path + '\tparams: ' + str(self.param_list)
+        
 
-class URLItemParser(object):
-    
-    def parse_item(self, l):
-        
+class URLModeltemParser(object):    
+    def do_naive_parse(self, log_item):
+        print 'do naive parser.\n'
         '''16/Oct/2014:18:20:57''' 
-        time    = l.split('[', 1)[1].split(']')[0]
-        formt_time = str(datetime.datetime.strptime(time, '%d/%b/%Y:%H:%M:%S'))
-        print 'format-time: ' + formt_time
+        time    = log_item.split('[', 1)[1].split(']')[0]
+        format_time = str(datetime.datetime.strptime(time, '%d/%b/%Y:%H:%M:%S'))
+        print 'format-time: ' + format_time
         
-        request = re.findall('"([^"]*)"', l)[0]
+        request = re.findall('"([^"]*)"', log_item)[0]
         mu  = request.split(' ')
         method  = mu[0]
         url     = mu[1]
+        
+        path, param_list    = self.parse_path_param_list(url)
+        
+        return URLModelItem(format_time, method, path, param_list)
+    
+    def do_formatter_parser(self, l, log_formatter, time_formatter):
+        print '\n------\n do formatter parser....'        
+#         print 'log formatter: ' + log_formatter
+#         print 'time formatter: ' + time_formatter
+        delimiter_list, format_string_pos = self.parse_log_format_string(log_formatter)
+#         print 'delimiter_list: ' + str(delimiter_list)
+#         print 'format string pos: ' + str(format_string_pos)        
+        tokens  = self.tokenize_log_item(l, delimiter_list)                      
+        time    = tokens[format_string_pos['t']]
+        method  = tokens[format_string_pos['m']]
+        url     = tokens[format_string_pos['u']]
+        
+        path, param_list    = self.parse_path_param_list(url)
+        
+        format_time = str(datetime.datetime.strptime(time, time_formatter))
+#         format_time = str(datetime.datetime.strptime(time, '%d/%b/%Y:%H:%M:%S'))
+#         print 'time: ' + time 
+#         print 'format_time: ' + format_time
+#         print 'method:' + method
+#         print 'url:' + url
+        
+        return URLModelItem(format_time, method, path, param_list)
+                
+    def parse_log_format_string(self, format_string):        
+        ''' log format string:
+            %m        method
+            %u        url, including query
+            %t        time
+            %-        placeholder
+        '''
+#         print 'parse format string;'
+        string_format_list      = []
+        content_list            = []
+        delimiter_list          = []
+        format_string_pos_dict  = {}
+        
+        flag = False
+        for c in format_string:
+            if not flag:
+                if c == '%':
+                    flag = True
+                elif c != ' ':
+                    delimiter_list.append(c)         
+                    string_format_list.append(c)  
+            elif flag:
+                if c != '-':
+                    format_string_pos_dict[c] = len(content_list)
+                    
+                content_list.append(c)                
+                string_format_list.append(c)
+                flag = False        
+        
+#         print string_format_list
+#         print content_list
+#         print delimiter_list
+#         print format_string_pos_dict
+        
+        return delimiter_list, format_string_pos_dict   
+    
+    def tokenize_log_item(self, log_item, delimiter_list): 
+#         delimiters = ' |\[|\"|\]'
+        delimiters = ' '
+        for d in delimiter_list:
+            delimiters += '|\\' + d
+#         print delimiters
+        
+        tokens = re.split(delimiters, log_item)
+        non_space_tokens = []
+        
+        for t in tokens:
+            if t :
+                non_space_tokens.append(t)
+        
+        return non_space_tokens
+        
+    def parse_path_param_list(self, url):
+        '''
+        :param url: full url including path and query string
+        '''
+        if '?' not in url:
+            return url, []
+        
         pq      = url.split('?')
         path    = pq[0]
         query   = pq[1]
         
-        param_value_pairs    = query.split("&")
+        if '=' not in query and '&' not in query:
+            return url, [query]
         
+        param_value_pairs   = query.split("&")         
         para_ordered_dict   = OrderedDict()
 #             para_dict           = dict() 
         for p in param_value_pairs:
             if "=" not in p: continue
-#             print '\np:' + p
             param, value = p.split("=", 1)
-            para_ordered_dict[param.strip()]=value.strip()
-            
+            para_ordered_dict[param.strip()]=value.strip()            
 #         print time + ': ' + method + ':\t' + path + '\t' 
 #         print para_ordered_dict.keys()  
+    
+        param_list = para_ordered_dict.keys()
         
-        url_item        = URLModelItem(formt_time, method, path, para_ordered_dict.keys())
-        nts_detector    = NTSDetector()
-        nts_detector.detect(url_item)
-
-class URLAlertTrainer(object):
-    '''
-    classdocs
-    '''
-
+        return path, param_list
+    
+class URLModelTrainer(object):
     def __init__(self, model_config_file):
-        mcb                 =  ModelConfigBuilder()
+        mcb                 = ModelConfigBuilder()
         config_file         = CONST.CONFIG_DIR + 'test-model.xml'
         self.model_config   = mcb.build_config(config_file)  
         model_directory     = CONST.MODEL_DIR + self.model_config.model_name
@@ -81,20 +159,24 @@ class URLAlertTrainer(object):
         last_model_file_number          = len(os.listdir(model_directory))
         self.latest_model_file_name     = CONST.MODEL_DIR + str(last_model_file_number) + '.xml'
         self.model_file_name            = CONST.MODEL_DIR + str(last_model_file_number + 1) + '.xml'
-        print 'last modle file: ' + self.latest_model_file_name
+        print 'last model file: ' + self.latest_model_file_name
         print 'model_file_name: ' + self.model_file_name
 
     def train_model(self):
         '''write the model'''
         with open(self.model_config.log_file) as f:
-            lines = f.readlines()
+            log_entries = f.readlines()
         
-        u_parser    = URLItemParser()
-        detector    = TSDetector()
-        for l in lines:
-#             print l + '\n\t'
-            item = u_parser.parse_item(l)
-            todo: detector TSDetector
+        nts_detector    = NTSDetector()
+        u_parser        = URLModeltemParser()
+        for log_entry in log_entries:            
+            model_item      = u_parser.do_formatter_parser(log_entry, self.model_config.log_formatter, self.model_config.time_formatter)
+            print 'log-entry: ' + log_entry + '\n\t'
+            print 'model_item:\t' + str(model_item)
+            
+            nts_detector.detect(model_item, log_entry)
+#             todo: update model file w/ this model_item
+
 
 class Detector(object):
     def alert_report(self, report):
@@ -102,22 +184,23 @@ class Detector(object):
         
 class NTSDetector(Detector):
     '''non time series model'''
-    def detect(self, url_item):
-        self.method_alert(url_item)
-        self.param_alert(url_item)
+    def detect(self, model_item, log_item):
+        print '****NTS dector***'
+        self.method_alert(model_item, log_item)
+        self.param_alert(model_item, log_item)
         
-    def method_alert(self, url_item):
+    def method_alert(self, model_item, log_item):
         '''check if insure method
         '''
-        method = url_item.method.upper() 
+        method = model_item.method.upper() 
         if method not in CONST.SECURE_HTTP_METHODS:
-            self.alert_report('insecure http method: ' + method + ' in request: ' + str(url_item))
+            self.alert_report('insecure http method: ' + method + ' in log item : ' + log_item)
     
-    def param_alert(self, url_item):
+    def param_alert(self, model_item, log_item):
         '''check params'''
-        for p in url_item.param_list:
+        for p in model_item.param_list:
             if self.sensitive_param(p):
-                self.alert_report('sensitive param name: ' + p + ' in request: ' + str(url_item))
+                self.alert_report('sensitive param name: ' + p + ' in log item : ' + log_item)
            
     def sensitive_param(self, param):
         return param.upper() in CONST.SENSITIVE_PARAM_NAMES
@@ -131,6 +214,12 @@ class TSDetector(Detector):
     def unsee_params(self, url_item):
         ''' check params '''
 
+# uip  = URLModeltemParser()
+# format_string = '%- %- %- [%t] "%m %u %-'
+# log_item = '182.236.164.11 - - [16/Oct/2014:18:20:58] "post /oldlink?itemId=EST-18&pAssWord=SD6SL8FF10ADFF53101 HTTP 1.1" 408 893 "http://www.buttercupgames.com/product.screen?productId=SF-BVS-G01" "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_4) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.46 Safari/536.5" 134'
+# uip.do_formatter_parser(log_item, format_string)
+
+
 config_file = CONST.CONFIG_DIR + 'test-model.xml'
-uat = URLAlertTrainer(config_file)
+uat = URLModelTrainer(config_file)
 uat.train_model()
